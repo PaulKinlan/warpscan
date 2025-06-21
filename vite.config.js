fd6d16e2570347ec299519999951e01fb48726ec
Paul Kinlan <paul.kinlan@gmail.com>
2025-06-21 23:43:13 +0100

    feat: add PWA support with VitePWA plugin and update icons
    - Added vite-plugin-pwa as a dependency in package.json.
    - Created multiple icon files for PWA in public/images.
    - Added an SVG icon for the application.
    - Removed unused icons.css file.
    - Updated vite.config.js to configure PWA settings including manifest and icon paths.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -15,4 +16,37 @@ export default defineConfig({
       },
     },
   },
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+
+      manifest: {
+        name: "WarpScan",
+        short_name: "WarpScan",
+        start_url: "/",
+        description: "Fun images scanning app",
+        scope: "/",
+        id: "/",
+        display: "standalone",
+        theme_color: "#00D0FF",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/images/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/images/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
